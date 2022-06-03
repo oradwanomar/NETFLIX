08227db06d40650a9c27911815740b0205ad42fd
oradwanomar <oradwan037@gmail.com>
2022-06-03 19:43:52 +0200

    update

--- NETFLIX/ViewController.swift
+++ NETFLIX/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemBackground
+        
     }
     
 }
