b516930541477e0953c67045de0b71cf0e7200c5
oradwanomar <oradwan037@gmail.com>
2022-06-03 19:38:10 +0200

    Begin

---
+++ NETFLIX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NETFLIX
+//
+//  Created by Omar Ahmed on 03/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
