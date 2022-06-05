edd5d09f803778bd7e27d1035f3b2145fb468a05
oradwanomar <oradwan037@gmail.com>
2022-06-05 02:40:57 +0200

    Update

--- NETFLIX/Controllers/MainTabBarViewController.swift
+++ NETFLIX/Controllers/MainTabBarViewController.swift
@@ -12,6 +12,7 @@ class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
+        
     }
     
     func configure(){
