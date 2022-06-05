7945645df85fc0b0d3fbc0e4608b7bc29948511f
oradwanomar <oradwan037@gmail.com>
2022-06-05 02:03:49 +0200

    Main Tabbar

--- NETFLIX/MainTabBarViewController.swift
+++ NETFLIX/MainTabBarViewController.swift
@@ -7,12 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
     }
     
 }
