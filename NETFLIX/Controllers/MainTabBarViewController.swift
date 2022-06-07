6b06fa68317937255ba28b22e3567a0c693a34c6
oradwanomar <oradwan037@gmail.com>
2022-06-07 22:58:52 +0200

    Update MainTabBarViewController.swift

--- NETFLIX/Controllers/MainTabBarViewController.swift
+++ NETFLIX/Controllers/MainTabBarViewController.swift
@@ -31,9 +31,6 @@ class MainTabBarViewController: UITabBarController {
         
         viewControllers = [homeVC,upcomingVC,searchVC,downloadsVC]
         tabBar.tintColor = .label
-        
-        
-        
 
     }
     
