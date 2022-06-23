2461c049fa09ef9736672e491c0b3ed95a6bfadf
omarradwan <oradwan037@gmail.com>
2022-06-23 21:09:14 +0200

    Update UpcomingViewController.swift

--- NETFLIX/Controllers/UpcomingViewController.swift
+++ NETFLIX/Controllers/UpcomingViewController.swift
@@ -16,6 +16,7 @@ class UpcomingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
     }
     
     //MARK: - Helpers
