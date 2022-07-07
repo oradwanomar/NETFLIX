df82bb84fe542466f6622955c0779eb2ab8a7ce6
omarradwan <oradwan037@gmail.com>
2022-07-07 19:48:49 +0200

    Update MainTabBarViewController.swift

--- NETFLIX/Controllers/MainTabBarViewController.swift
+++ NETFLIX/Controllers/MainTabBarViewController.swift
@@ -19,7 +19,6 @@ class MainTabBarViewController: UITabBarController {
     
     func configureMainTabBar(){
         
-        
         view.backgroundColor = .systemBackground
         
         let homeVC = setUpMainTabBarNavigationController(selectedImage: UIImage(systemName: "house.fill")!, unselectedImage: UIImage(systemName: "house")!, viewController: HomeViewController(), title: "Home")
